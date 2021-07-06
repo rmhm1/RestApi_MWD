f4c2704b1eb3e754ded4375b11feea3821367859
Ryland <rmhm1@vt.edu>
2021-07-06 18:08:20 -0400

    Attempt at fixing create_all. Moved call to manage

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_name):
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
 
-    with app.app_context():
-        db.create_all()
-        return app
+    # with app.app_context():
+     #   db.create_all()
+
+    return app
--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@ import os
 # from main import create_app, db
 from app.main import create_app, db
 app = create_app(os.getenv('ENV') or 'prod')
+app.app_context().push()
+db.create_all()
+
 
 if __name__ == '__main__':
     app.run()
