56d3cbc13a0454f378dd6416b87dd94a944d24d0
Ryland <rmhm1@vt.edu>
2021-07-06 19:25:55 -0400

    Added create_all to run section

--- manage.py
+++ manage.py
@@ -2,9 +2,8 @@ import os
 # from main import create_app, db
 from app.main import create_app, db
 app = create_app(os.getenv('ENV') or 'prod')
-app.app_context().push()
-db.create_all()
 
 
 if __name__ == '__main__':
     app.run()
+    db.create_all()
