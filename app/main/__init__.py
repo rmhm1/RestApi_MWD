83193841151a0020c71cd85516f720d0d118e9df
Ryland <rmhm1@vt.edu>
2021-07-06 20:16:12 -0400

    Moved import of routes into app context

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -14,7 +14,7 @@ def create_app(config_name):
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
 
-    from . import MWD_Resources
     with app.app_context():
+        from . import MWD_Resources
         db.create_all()
         return app
