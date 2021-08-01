236435d442675dc67c66da857dc7d2f3fa1110f1
Ryland <rmhm1@vt.edu>
2021-08-01 16:57:07 -0400

    Updated all features sizing

--- app/Resources/plotting.py
+++ app/Resources/plotting.py
@@ -111,7 +111,7 @@ def plot_all_features(df, holeID):
     specific_df = df[df.holeID == holeID]  # Grabs the entries of the specific holeID
     depth = specific_df.Depth.max() - specific_df.Depth.min()  # Calculates the overall depth
     temp_dict = {'holeID': holeID, 'depth': depth}  # Creates a dictionary for each holeID
-    fig, ax = plt.subplots(figsize = (5, 12))  # Generate the figure and axis before loop for reuseability for efficiency
+    fig, ax = plt.subplots(figsize = (5, 13))  # Generate the figure and axis before loop for reuseability for efficiency
 
     for feature in df.columns[1:6]:  # Loop over all relevant features
         ax.plot(specific_df[feature].values, specific_df.Depth, color=color_dict[feature])
